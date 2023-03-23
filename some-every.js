d1c614f1393db96637bbb52e08d03b2aacb8786d
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-03-23 09:33:27 -0400

    feat: Created function hasCertainValue
    hasCertainValue accept an array of object and a key and returns true if every single obect in the array contains that value for a specfic key.

--- some-every.js
+++ some-every.js
@@ -93,4 +93,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(obj){
+        return obj[key] === searchValue
+    })
+}
